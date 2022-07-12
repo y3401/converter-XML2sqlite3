006f84f03ce8641a04c746d466d2ebc9086d6bff
Yury <y3401@yandex.ru>
2022-07-12 08:30:21 +0300

    Update xml2sqlite3.py
    добавлен новый формат выгрузки infotor-2

--- xml2sqlite3.py
+++ xml2sqlite3.py
@@ -1,15 +1,17 @@
+#
 import tkinter as tk 
-from tkinter.filedialog import askopenfilename, asksaveasfilename, askdirectory
-from tkinter.ttk import Combobox, Checkbutton, Progressbar, Entry, Label
+from tkinter.filedialog import askopenfilename, askdirectory
+from tkinter.ttk import Checkbutton, Progressbar, Entry, Label
 from tkinter import messagebox as mbox
 from tkinter import BooleanVar
-from xml.dom import minidom
+#from xml.dom import minidom
 import os, os.path
 import modsql3_ as lite
 import export_in_CSV as expCSV
 import time
 import re
 import pickle
+import threading
 
 
 pickle.DEFAULT_PROTOCOL
@@ -26,8 +28,11 @@ fr = ''
 n=0
 f_list = []
 tid=''
+stp=True
+started=False
+stime = True
 
-            
+
 def calc(bsize=0):
     it=0
     razmer=['б','Кб','Мб','Гб','Тб']
@@ -62,8 +67,8 @@ def load_param():
         path_DB.insert(0,param.get('dirDB','C:/DB'))
         chk_state.set(param.get('vacuum', True))
     except:
-        pass
-    
+        output = open('param.pkl', 'wb')
+        output.close()
     
 def init_start():
     k=0
@@ -82,14 +87,13 @@ def select_file():
     up_select_file(filepath)
 
 def SelectDir():
-    dir_name = askdirectory(initialdir='C:')
+    dir_name = askdirectory(initialdir=os.getcwd())
     if dir_name == "":
         return None
     else:
         path_DB.delete(0,tk.END)
-        if dir_name[-1:] != '/': dir_name = dir_name + '/'
-        if dir_name[-3:] != 'DB/': dir_name = dir_name + 'DB'
         path_DB.insert(0,dir_name)
+        check_exist()
         
 def up_select_file(fl):
     global period, sz, catalog
@@ -102,110 +106,92 @@ def up_select_file(fl):
         period = re.findall(r'\d{8}', fl)[0]
     except:
         period = str(time.localtime()[0])+('0'+str(time.localtime()[1]))[-2:]+('0'+str(time.localtime()[2]))[-2:]
-    #period = fl.split(".")[1][:8]
     catalog = './'+period
     window.title(f"XML > SQLite3 - {period}")
     init_start()
-    #bar['value'] = 0
-    #lbl_percent.config(text = "0 %")
-    #lbl_status.config(text="")
-    #lbl_progress.config(text="0")
     k = 0
 
-def ttt(childList):
-    global n, f_list,tid
-    for c in childList:
-        if c.nodeType!=c.TEXT_NODE:
-            if c.nodeName=='dir':
-                fileName = c.attributes.item(0).value
-                n += 1
-                f_list.append((tid, fileName,0,n))
-            elif c.nodeName=='file':
-                fileName = c.attributes.getNamedItem("name").value
-                fileSize = c.attributes.getNamedItem("size").value
-                n += 1
-                f_list.append((tid, fileName,fileSize,n))
-            cc = c.childNodes
-            ttt(cc)
+def check_exist():                      # проверка наличия torrents2.db3
+    bd = path_DB.get() + "/torrents2.db3"
+    if os.path.exists(bd):
+        if os.path.getsize(bd)>500000000:
+            btn_csv['state']='normal'
 
-def extract(buff):
-    global k,n,f_list,tid
-    
-    mydoc= minidom.parseString(buff)
-    mydoc.normalize()
-    node1 = mydoc.getElementsByTagName("torrent")[0]
-    tid = node1.getAttribute("id")
-    reg_date = node1.getAttribute("registred_at")
-    b_size = node1.getAttribute("size")
-    tl = mydoc.getElementsByTagName("title")[0]
-    title = tl.childNodes[0].nodeValue
-    title=title.replace('"',"'")
-    node2 = mydoc.getElementsByTagName("torrent")[1]
-    magnet = node2.getAttribute("hash")
-    frm = mydoc.getElementsByTagName("forum")[0]
-    forum_id = frm.getAttribute("id")
-    forum = frm.childNodes[0].nodeValue
-    forum=forum.replace('"',"'")
-    cntnt = mydoc.getElementsByTagName("content")[0]
-    content = cntnt.childNodes[0].nodeValue
-    k += 1
-    n=CAT_INV.get(forum.split(sep=" - ")[0],0)
-    # если n=0 тогда внести в базу
-    fr=forum.split(sep=" - ")[-1]
-    if forums.get(forum_id,0) == 0: #добавление в словарь и в базу
-        forums[forum_id] = ''
-        lite.check_podr(forum_id,fr,n)  
-    lite.ins_tor(forum_id,tid,magnet,title,b_size,reg_date)
-    lite.ins_content(tid,content)
-
+def readFileList(tid,lines):            # читаем список файлов
     n = 0
-    f_list = []
-    nodedir = mydoc.childNodes
-    ttt(nodedir)
-    lite.ins_files(tid,f_list)
-    
-'''    nodedir = mydoc.getElementsByTagName("dir")
-    if len(nodedir)!=0:
-        ndir = mydoc.getElementsByTagName("dir")[0]
-        dirName = ndir.attributes.item(0).value
-        n += 1
-        a = (tid, dirName,0,n)
-        f_list.append(a)
-        childList=ndir.childNodes
-        for child in childList:
-            if child.nodeType!=child.TEXT_NODE:
-                if child.nodeName=='dir':
-                    fileName = child.attributes.item(0).value
-                    n += 1
-                    a = (tid, fileName,0,n)
-                    f_list.append(a)
-                elif child.nodeName=='file':
-                    fileName = child.attributes.getNamedItem("name").value
-                    fileSize = child.attributes.getNamedItem("size").value
-                    n += 1
-                    a = (tid, fileName,fileSize,n)
-                    f_list.append(a)
-                cnodes=child.childNodes
-                for cn in cnodes:
-                    if cn.nodeType!=cn.TEXT_NODE:
-                        if cn.nodeName=='file':
-                            fileName = cn.attributes.getNamedItem("name").value
-                            fileSize = cn.attributes.getNamedItem("size").value
-                            n += 1
-                            a = (tid, fileName,fileSize,n)
-                            f_list.append(a)      
-    else:
-        nodefile = mydoc.getElementsByTagName("file")
-        if len(nodefile)!=0:
-            nfile = mydoc.getElementsByTagName("file")[0]
-            fileName = nfile.attributes.getNamedItem("name").value
-            fileSize = nfile.attributes.getNamedItem("size").value
-            n += 1
-            a = (tid, fileName,fileSize,n)
-            f_list.append(a)
+    f_list = ''
 
-    lite.ins_files(f_list)
-'''
+    relines = re.findall(r"<.*?>",lines,re.S)
+    for line in relines:
+        
+        line = line.replace("\n"," ")
+        if line.startswith("<dir",0):
+            named = re.findall(r'''name="(.*?)">''',line,re.S+re.MULTILINE)
+            try:
+                fileName = named[0]
+            except:
+                pass
+            n += 1
+            f_list += ',("{0}","{1}",0,{2})'.format(tid,fileName,n)
+        elif line.startswith("<file",0):
+            namef = re.findall(r'''name="(.*?)"/>''',line,re.S+re.MULTILINE)
+            try:
+                fileName = namef[0]
+            except:
+                pass
+            sizef = re.findall(r'size="(.+)"\s',line,re.S)
+            fileSize = sizef[0]
+            n += 1
+            f_list += ',("{0}","{1}","{2}",{3})'.format(tid, fileName,fileSize,n)
+    return tid,f_list
+
+def extract(buff):                          # Парсер. DOM заменен на re - быстрее работает 
+    tids=re.findall(r'torrent\sid="(.*?)\"',buff)
+    tid=tids[0]
+    rd = re.findall(r'registred_at="(.*?)\"',buff)
+    reg_date = rd[0]
+    sizes = re.findall(r'size="(.*?)\">',buff)
+    b_size = sizes[0]
+
+    tl = re.findall(r"<title>(.*?)</title>",buff,re.MULTILINE+re.S)
+    titles = tl[0]
+    if titles.find('CDATA')>-1:
+        title=titles[9:-3]
+    else:
+        title = titles
+    title=title.replace('"',"'")
+    mg = re.findall(r'hash=\"(.*?)\"',buff)
+    magnet = mg[0]
+    frm = re.findall(r'forum\sid="(.*?)\">',buff)
+    forum_id = frm[0]
+    nn = len(forum_id) +13
+    forums = re.findall(r'<forum .*?forum>',buff)
+    ftmp = forums[0][nn:-8]
+    
+    if ftmp.find('CDATA')>-1:
+        forum=ftmp[9:-3]
+    else:
+        forum = ftmp
+    invers_category()
+    forum=forum.replace('"',"'")
+    n=CAT_INV.get(forum.split(sep=" - ")[0],0)
+    fr=forum.split(sep=" - ")[-1]
+
+    cnt = re.findall(r"<content>(.*?)</content>",buff,re.MULTILINE+re.S)
+    contents= cnt[0]
+    if contents.find('CDATA')>1:
+        content = contents[9:-3]
+    else:
+        content = contents
+    
+    fls = re.findall(r"/content>(.*?)</tor",buff,re.MULTILINE+re.S)
+    dirs = fls[0] 
+
+    return ([forum_id,fr,n,tid,magnet,title,b_size,reg_date,content,('[{0}]'.format(readFileList(tid,dirs)[1][1:]))])
+    #return ([forum_id,fr,n,tid,magnet,title,b_size,reg_date])   
+    # forum_id,fr,n,tid,magnet,title,b_size,reg_date,content,'[{0}]'.format(readFileList(tid,dirs)[1][1:]) 
+    #    0     1  2   3    4      5    6        7        8          9   
+
 
 def invers_category():
     for la in lite.CAT:
@@ -215,71 +201,81 @@ def show_status(message):
     lbl_status.config(text=message)
     window.update()
 
-'''def sectXML(file):
-    k = 1
-    size_buff = 500000000 
-    buff = ''
-    buff1 = ''
-
-    with open(file,"r",encoding='utf8') as fn:
-        buff= fn.read(size_buff)
-           
-        while len(buff)>0:
-            
-            line= fn.readline()
-            if not line:
-                fout = open("1/" + file + "." + str(k),"w",encoding='utf8',newline ='\n')            
-                fout.write(buff)
-                fout.close()
-                show_status(str(k))
-                break
-            #if not line.startswith('<torrent '): 
-            buff1 += line
-            if line.startswith('</torrent>'):
-                fout = open("1/" + file + "." + str(k),"w",encoding='utf8',newline ='\n')            
-                fout.write(buff + buff1)
-                fout.close()
-                show_status(str(k))
-                k += 1
-                buff = ''
-                buff1 = ''
-                buff= fn.read(size_buff)
-            
-                 
-    fn.close()
-'''
-def readXML(file):
+def readXML(file):                  # тред чтения xml-файла и записи в базы
+    global k,stime,stp,started
+    poz=0
     key = 0
     buff = ''
-     
-
+    invers_category()
+    stp=False
+    #t2 = threading.Thread(target=recording,args=(Q,))
+    #t2.daemon=True    
+    #t2.start()
+##
+    #lite.create_db(dirDB+'/')
+    #lite.create_db_2(dirDB+'/')
+    #lite.create_db_content(dirDB+'/')
+    #lite.ins_vers(period)
+    #time.sleep(0.1)
+    #pool = multiprocessing.Pool(3)
+##
     with open(file,encoding='utf8') as fn:
         
         while True:
+            
             line= fn.readline()
+            
             if not line:
                 break
             if line.startswith('<torrent '):
                 key=1
             if line.startswith('</torrent>') and key == 1:
                 buff += line
-                extract(buff)
+                poz=fn.tell()
+                if started == False:
+                    lite.dbc()
+                    lite.close_db(0)
+                    fn.close()
+                    window.config(cursor="")
+                    btn_start.config(text="Старт")
+                    stime=False
+                    stp=True
+                    break
+                #Q.put(buff)
+                try:
+                    #res=pool.apply_async(extract,args=(buff,))
+                    res = extract(buff)
+                except:
+                    pass
+                else:
+                    data=res #.get()
+                    lite.check_podr(data[0],data[1],data[2])
+                    lite.insert_tor(data[2],data[0],data[3],data[4],data[5],data[6],data[7])    # torrents2.db3
+                    lite.ins_tor(data[0],data[3],data[4],data[5],data[6],data[7])               # torrents.db3
+                    lite.ins_content(data[3],data[8],data[9])                                   # content.db3
+                lb = len(buff)
                 buff = ''
                 key=0
+                k += 1
                 if k%1000==0:
                     lite.dbc()
                     lbl_progress.config(text = sepp(k))
-                    window.update()
-        
-                if k%5000==0:
-                    perc=round(100*fn.tell()/sz,3)
+                    perc=round(100*poz/sz,3)
                     bar['value'] = perc
                     lbl_percent.config(text = str(perc) + " %")
                     window.update()
-              
+                
+
             if key==1:
-                buff += line 
+                buff += line
+    
+    perc=round(100*(poz+12)/sz,2)
+    bar['value'] = perc
+    lbl_percent.config(text = str(perc) + " %")
     fn.close()
+    stp=True
+    lite.dbc()
+    stime=False
 
 def xml_test(file):
     if file=="":
@@ -298,7 +294,7 @@ def xml_test(file):
             btn_start['state']='disabled'
             return False
 
-def exportCSV():
+def exportCSV():                                # Экспорт в CSV
     dirDB = path_DB.get() + '/'
     period = expCSV.DBvers(dirDB)
     catalog = dirDB + period
@@ -322,55 +318,106 @@ def exportCSV():
     show_status('Выгрузка списка форумов forums.csv')
     show_status('Готово!')
     mbox.showinfo("Обработка","Готово!")
+
+def vremya(tsec):                       # приведение времени к удобочитаемому виду
+    tsec=int(tsec)
+    seconds=tsec % 60
+    minutes=(tsec//60) % 60
+    hours=(tsec//3600) % 24
+    return ("{0}:{1}:{2}".format(str(hours),('0'+str(minutes))[-2:],('0'+str(seconds))[-2:]))
+
+def time_status(tb,infile):             # подсчет времени исполнения 
+    global stime
+    while True:
+        if started == False:
+            break
+        if stime == False: 
+            break
+        var=vremya(round(time.time() - tb))
+        lbl_status.config(text=f"Обработка файла {infile} - " + var)
+        window.update()
+        time.sleep(.1)
+        
+def thread1(infile):
+    global k,time_begin,stime,started
+    #pid=os.getpid()
+    #print(pid)
+    #if started==False:
+    #    os.kill(pid,3)
+    #btn_start['state']='disabled'
+    if not os.path.exists(dirDB):
+        os.mkdir(dirDB)
+    window.config(cursor="wait")
     
-def start():
-    global k
+    lite.open_db(dirDB+'/')
+    lite.open_db_2(dirDB+'/')
+    lite.open_db_content(dirDB+'/')
+     
+    readXML(infile)
+    lbl_progress.config(text = sepp(k))   
+    par = 0
+    if chk_state.get()==True:
+        par = 1
+        show_status("Сжатие баз")
+    lite.close_db(par)
+        
+    tsec=time.time()-time_begin
+    window.config(cursor="")
+    started=False
+    show_status('Затраченное время - ' + vremya(tsec) + ' Обработано {0} записей'.format(sepp(k)))
+    k = 0
+    #bar['value'] = 100
+    #lbl_percent.config(text = "100 %")
+    btn_csv['state']='normal'
+    btn_start.config(text="Старт")
+    window.update()
+
+def starts(event):                        # Запуск основного процесса и таймера
+    global k,time_begin,infile,started
+    
     init_start()
     infile = xmlfile.get()
     if xml_test(infile):
         show_status('Создание БД')
-        save_param()
-        if not os.path.exists(dirDB):
-            os.mkdir(dirDB)
-        lite.create_db(dirDB+'/') 
-        lite.create_db_content(dirDB+'/')
-        lite.ins_vers(period)
+        save_param()        
         time_begin=time.time()
         invers_category()
-        show_status(f"Обработка файла {infile}")
-        #sectXML(xmlfile.get())
-        readXML(xmlfile.get())
-        lite.dbc()
-        lbl_progress.config(text = sepp(k))
-        #btn_start['state']='disabled'
-        window.update()
-        par = 0
-        if chk_state.get()==True:
-            par = 1
-            show_status("Сжатие баз")
-        lite.close_db(par)
-        time_end=time.time()
-        tsec=time_end-time_begin
-        stsec=(str(tsec)).split('.')
-        tsec=int(stsec[0])
-        seconds=0
-        minutes=0
-        hours=0
-        n=0
-        seconds=tsec % 60
-        minutes=(tsec//60) % 60
-        hours=(tsec//3600) % 24
-        show_status('Затраченное время - %s:%s:%s Обработано %s записей' % (str(hours),('0'+str(minutes))[-2:],('0'+str(seconds))[-2:], sepp(k)))
-        k = 0
-        bar['value'] = 100
-        lbl_percent.config(text = "100 %")
-        btn_csv['state']='normal'
+        btn_start.config(text="Стоп")
+        btn_start.unbind('<ButtonPress>')
+        btn_start.bind('<ButtonPress>',stops)
+        started=True
+        t = threading.Thread(target=time_status,args=(time_begin,infile))
+        t.daemon=True
+        t.start()
+        lite.create_db(dirDB+'/')
+        lite.create_db_2(dirDB+'/')
+        lite.create_db_content(dirDB+'/')
+        lite.ins_vers(period)
+        lite.close_db(0)
+        t1 = threading.Thread(target=thread1,args=(infile,))
+        t1.start()
+        #t1.join()
+        if t1.is_alive==True:
+            while 1:
+                if t1.is_alive==False or started == False:
+                    break
     else:
         pass
-    
-def show_window():
+
+def stops(event):
+    global started,stime
+    started=False
+    stime=False
+    btn_start.unbind('<ButtonPress>')
+    btn_start.bind('<ButtonPress>',starts)
+
+
+
+
+def show_window():                      # главное окно программы
     global window, xmlfile, path_DB, btn_start, lbl_size, chk, lbl_progress, bar, lbl_percent, lbl_status, btn_csv
     global chk_state
+    
     window = tk.Tk()
     window.title("XML > SQLite3")
     x = (window.winfo_screenwidth() - 450)/2
@@ -408,9 +455,11 @@ def show_window():
     btn_dir = tk.Button(window, image=photo2, command=SelectDir, width=15)
     btn_dir.grid(column=2, row=2, padx=0, pady=5, sticky="w")
 
-    btn_start = tk.Button(window, text="Старт", command=start)
+    btn_start = tk.Button(window, text="Старт",cursor="")
     btn_start.grid(column=3, row=3, padx=5, pady=5, sticky="ew")
+    btn_start.bind('<ButtonPress>',starts)
     btn_start['state']='disabled'
+    
 
     chk_state = BooleanVar()  
     chk_state.set(True)  # 
@@ -433,12 +482,11 @@ def show_window():
 
     statusbar = Entry(window,width=100)
     statusbar['state'] = 'readonly'
-    #statusbar.bind("<Key>", lambda e: "break")
     statusbar.grid(column=0,columnspan=4, row=6,padx=0,pady=20)
 
     lbl_status = Label(window, text="")  
-    lbl_status.grid(column=0,columnspan=3, row=6, padx=0, pady=20,sticky="w")
-
+    lbl_status.grid(column=0,columnspan=5, row=6, padx=0, pady=20,sticky="w")
+    
     backup=''
     for f in os.listdir('.'):
         if (f[:8] == 'backup.2' or f[:10] == 'rutracker-') and f[-3:] == 'xml':
@@ -447,12 +495,11 @@ def show_window():
     if len(backup)>0:
         up_select_file(backup)
     load_param()
-    
-    window.mainloop()
+    check_exist()
 
+    window.mainloop()
 
 # START PROG ->
 if __name__ == '__main__':
-
+    
     show_window()
-
